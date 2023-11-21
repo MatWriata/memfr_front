9556068085139dd242f33c7669b7eb584690632a
MatWriata <matwriata@gmail.com>
2023-11-21 23:36:43 +0100

    bricolage

---
+++ memfr_front/src/Components/MainPage/Index.tsx
@@ -0,0 +1,12 @@
+import {FormControl, FormHelperText, Input, InputLabel} from "@mui/material";
+
+
+export function Index()
+{
+    return (<FormControl>
+            <InputLabel htmlFor="my-input">Email address</InputLabel>
+            <Input id="my-input" aria-describedby="my-helper-text" />
+            <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+        </FormControl>
+    )
+}
